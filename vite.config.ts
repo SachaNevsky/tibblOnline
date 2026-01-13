4ce3a6728d0ee85d0b0ed91017bdc234fdb47130
SachaNevsky <sacha.nevsky@gmail.com>
2026-01-13 21:54:16 +0100

    gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: "/tibblOnline/",
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
